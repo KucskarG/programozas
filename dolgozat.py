39de43e391606c3b7249f962d75b3bcaef89df68
KucskarG <90601194+KucskarG@users.noreply.github.com>
2021-10-20 14:15:50 +0200

    Add files via upload

---
+++ dolgozat.py
@@ -0,0 +1,15 @@
+a = int (input('Irjon be egy számot '))
+b = int (input('Irjon mégegy számot '))
+print('A három szám összege: ', a+b)
+
+a2 = int (input('Irjon be egy számot '))
+b2 = int (input('Irjon mégegy számot '))
+print('A három szám különbsége: ', a2-b2)
+
+a3 = int (input('Irjon be egy számot '))
+b3 = int (input('Irjon mégegy számot '))
+print('A három szám szorzata: ', a3*b3)
+
+a1 = int (input('Irjon be egy számot '))
+b2 = int (input('Irjon mégegy számot '))
+print('A három szám hányadosa: ', a1/b2)
