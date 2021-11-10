151a5fdc3ae83b2f479454dd1997c17d12a15085
KucskarG <90601194+KucskarG@users.noreply.github.com>
2021-11-10 14:36:56 +0100

    Add files via upload

---
+++ szamolosak.py
@@ -0,0 +1,34 @@
+print ('Vőlegényem, drága vagy az én szívemnek,')
+print ('Ragyogó a te szépséged, édes vagy, mint a méz,')
+print ('Oroszlánom, drága vagy az én szívemnek,')
+print ('Ragyogó a te szépséged, édes vagy, mint a méz,')
+
+a1 = (input('Hogy hívják: ')) 
+a2= int (input('Mikor született: '))
+a3= (input('Hol született: '))
+a4= (input('Jelenlegi lakcíme: '))
+print ('Magát ' , a1 , '-nak hívják')
+print ( 'Maga ' ,a2, '-kor született' )
+print ( 'Maga ' ,a3, ' ban/ben született' )
+print ( 'A maga lakcíme: ' ,a4,)
+
+b1=int (input('Irj be egy számot '))
+b2=int (input('Irj be mégegyszámot '))
+print ('Előtte ' ,b1, 'és' ,b2, )
+print ('Utána ' ,b2, 'és' ,b1,)
+
+c1=int (input('Irjon egy számot: '))
+c2=int (input('Irjon  mégegy számot: '))
+print ('Terület ' ,c1*c2)
+print ('Kerület ' ,2*(c1+c2))
+
+b1=int (input('Irjon egy számot: '))
+b2=int (input('Irjon  mégegy számot: '))
+b3=int (input('Irjon  mégegy számot: '))
+print ('Terület ' ,b1*b2/2)
+print ('Kerület ' ,b1+b2+b3)
+
+
+
+
+
