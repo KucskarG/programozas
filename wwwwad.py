4c3833dc80593cbbf3b3632063578420e19bb9b5
KucskarG <90601194+KucskarG@users.noreply.github.com>
2022-01-20 11:28:59 +0100

    Add files via upload

---
+++ wwwwad.py
@@ -0,0 +1,12 @@
+from operator import length_hint
+
+
+anaa = str (input("Irj be egy szót"))
+anaaa =str (input("Irj be egymásik szót"))
+anaa_hosz=len (anaa)
+anaaa_hosz=len (anaaa)
+if (anaa_hosz > anaaa_hosz)
+    print ("első a hosszab")
+elif (anaaa_hosz > anaa_hosz)
+    print("második a hoszabb")
+else("a két szó egyforma")
